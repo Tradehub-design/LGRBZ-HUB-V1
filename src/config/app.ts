e266f6a892c81caba00166a43e17ab5f39fd4058
Tradehub-design <kierenchan1@gmail.com>
2026-07-07 00:51:14 +1000

    Create app.ts

---
+++ src/config/app.ts
@@ -0,0 +1,15 @@
+export const appConfig = {
+  name: "Portfolio Performance",
+
+  version: "1.0.0",
+
+  currency: "AUD",
+
+  timezone: "Australia/Sydney",
+
+  supportEmail:
+    "support@portfolio-performance.com",
+
+  environment:
+    process.env.NODE_ENV,
+};
