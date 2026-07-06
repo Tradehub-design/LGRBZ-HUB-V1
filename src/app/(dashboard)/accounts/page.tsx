90f5ee630cc4954f70b13ca9d08f7c987e8c66ee
Tradehub-design <kierenchan1@gmail.com>
2026-07-06 22:40:03 +1000

    Create page.tsx

---
+++ src/app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,9 @@
+import { AccountsHeader } from "@/features/accounts/components/accounts-header";
+
+export default function AccountsPage() {
+  return (
+    <div className="space-y-6">
+      <AccountsHeader />
+    </div>
+  );
+}
