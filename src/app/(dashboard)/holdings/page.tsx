88f10ae28433a7c4755cf9ced47e88e64fd8ab1d
Tradehub-design <kierenchan1@gmail.com>
2026-07-06 21:59:31 +1000

    Create page.tsx

---
+++ src/app/(dashboard)/holdings/page.tsx
@@ -0,0 +1,9 @@
+import { HoldingsHeader } from "@/features/holdings/components/holdings-header";
+
+export default function HoldingsPage() {
+  return (
+    <div className="space-y-6">
+      <HoldingsHeader />
+    </div>
+  );
+}
