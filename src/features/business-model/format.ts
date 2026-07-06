a8b2407d03fba27633b28c4c95c3b758d7d4e81c
Tradehub-design <kierenchan1@gmail.com>
2026-07-06 23:12:38 +1000

    Create format.ts

---
+++ src/features/business-model/format.ts
@@ -0,0 +1,7 @@
+export function formatRiskMoney(value: number) {
+  return new Intl.NumberFormat("en-AU", {
+    style: "currency",
+    currency: "AUD",
+    maximumFractionDigits: 2,
+  }).format(value);
+}
