bd8efdf3ffb340e3dea7f5b6a22d49d7589e914a
Tradehub-design <kierenchan1@gmail.com>
2026-07-07 01:12:18 +1000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,11 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class",
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/providers/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/store/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {}
